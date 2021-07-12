59153161ed8f571a9b8fca77f9f59f26b89f27cd
Rebbit13 <errb13@gmail.com>
2021-07-12 17:45:14 +0300

    refactor tests

--- tests/test_task.py
+++ tests/test_task.py
@@ -5,221 +5,139 @@ from app import app
 from models.task import Task
 from peewee import SqliteDatabase
 
+app.testing = True
+
 test_db = SqliteDatabase(":memory:")
 
 
-class TaskTestCase(TestCase):
-    task_path = "/api/task/"
-    task_path_id_1 = "/api/task/1/"
-    task_path_id_100 = "/api/task/100/"
-
-    correct_json = {"title": "some title",
-                    "content": "some content"}
-
-    missing_argument_title = {"content": "some content"}
-    missing_argument_content = {"title": "some title"}
-    missing_arguments_all = {}
-
-    title_less_chars = {"title": "",
-                        "content": "some content"}
-    content_less_chars = {"title": "some title",
-                          "content": ""}
-
-    title_more_chars = {"title": "s" * 151,
-                        "content": "some content"}
-
-    typo_title = {"tutle": "some title",
-                  "content": "some content"}
-    typo_content = {"title": "some title",
-                    "cuntent": "some content"}
-
-    def _set_up(self):
+class TaskTest(TestCase):
+    def setUp(self):
         self.app = app.test_client()
         test_db.bind([Task], bind_refs=False, bind_backrefs=False)
         test_db.connect()
         test_db.create_tables([Task])
-        self.app.post("/api/task/",
-                      json=self.correct_json)
+        self.test_task = Task(title="title", content="content")
+        self.test_task.save()
 
-    @staticmethod
-    def _tear_down():
+    def tearDown(self):
+        self.app.delete()
         test_db.drop_tables([Task])
         test_db.close()
 
-
-class PostTaskTestCase(TaskTestCase):
-
     def test_post_success(self):
-        self._set_up()
-        r = self.app.post(self.task_path,
-                          json=self.correct_json)
+        r = self.app.post("/api/task/",
+                          json={"title": "some title", "content": "some content"})
         self.assertEqual(r.status_code, 201)
-        self._tear_down()
 
     def test_post_missing_argument_title(self):
-        self._set_up()
-        r = self.app.post(self.task_path,
-                          json=self.missing_argument_title)
+        r = self.app.post("/api/task/",
+                          json={"content": "some content"})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
     def test_post_missing_argument_content(self):
-        self._set_up()
-        r = self.app.post(self.task_path,
-                          json=self.missing_argument_content)
+        r = self.app.post("/api/task/",
+                          json={"title": "some title"})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
     def test_post_missing_arguments_all(self):
-        self._set_up()
-        r = self.app.post(self.task_path,
-                          json=self.missing_arguments_all)
+        r = self.app.post("/api/task/",
+                          json={})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
     def test_post_title_less_chars(self):
-        self._set_up()
-        r = self.app.post(self.task_path,
-                          json=self.title_less_chars)
+        r = self.app.post("/api/task/",
+                          json={"title": "", "content": "some content"})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
     def test_post_content_less_chars(self):
-        self._set_up()
-        r = self.app.post(self.task_path,
-                          json=self.content_less_chars)
+        r = self.app.post("/api/task/",
+                          json={"title": "some title", "content": ""})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
     def test_post_title_more_chars(self):
-        self._set_up()
-        r = self.app.post(self.task_path,
-                          json=self.title_more_chars)
+        r = self.app.post("/api/task/",
+                          json={"title": "s" * 151, "content": "some content"})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
     def test_post_typo_title(self):
-        self._set_up()
-        r = self.app.post(self.task_path,
-                          json=self.typo_title)
+        r = self.app.post("/api/task/",
+                          json={"tutle": "some title", "content": "some content"})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
     def test_post_typo_content(self):
-        self._set_up()
-        r = self.app.post(self.task_path,
-                          json=self.typo_content)
+        r = self.app.post("/api/task/",
+                          json={"title": "some title", "cuntent": "some content"})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
-
-
-class GetTaskTestCase(TaskTestCase):
 
     def test_get_all(self):
-        self._set_up()
-        r = self.app.get(self.task_path)
+        r = self.app.get("/api/task/")
         self.assertEqual(r.status_code, 200)
-        self._tear_down()
 
     def test_get_success(self):
-        self._set_up()
-        r = self.app.get(self.task_path_id_1)
+        r = self.app.get("/api/task/1/")
         self.assertEqual(r.status_code, 200)
-        self._tear_down()
 
     def test_get_fail(self):
-        self._set_up()
-        r = self.app.get(self.task_path_id_100)
+        r = self.app.get("/api/task/100/")
         self.assertEqual(r.status_code, 404)
-        self._tear_down()
-
-
-class DeleteTaskTestCase(TaskTestCase):
 
     def test_delete_success(self):
-        self._set_up()
-        r = self.app.delete(self.task_path_id_1)
+        r = self.app.delete("/api/task/1/")
         self.assertEqual(r.status_code, 200)
-        self._tear_down()
 
     def test_delete_fail(self):
-        self._set_up()
-        r = self.app.delete(self.task_path_id_100)
+        r = self.app.delete("/api/task/100/")
         self.assertEqual(r.status_code, 404)
-        self._tear_down()
-
-
-class PutTaskTestCase(TaskTestCase):
 
     def test_put_success(self):
-        self._set_up()
-        r = self.app.put(self.task_path_id_1,
-                         json=self.correct_json)
+        r = self.app.put("/api/task/1/",
+                         json={"title": "some title", "content": "some content"})
         self.assertEqual(r.status_code, 201)
-        self._tear_down()
 
     def test_put_fail_not_found(self):
-        self._set_up()
-        r = self.app.put(self.task_path_id_100,
-                         json=self.correct_json)
+        r = self.app.put("/api/task/100/",
+                         json={"title": "some title", "content": "some content"})
         self.assertEqual(r.status_code, 404)
-        self._tear_down()
 
     def test_put_missing_argument_title(self):
-        self._set_up()
-        r = self.app.put(self.task_path_id_1,
-                         json=self.missing_argument_title)
+        r = self.app.put("/api/task/1/",
+                         json={"content": "some content"})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
     def test_put_missing_argument_content(self):
-        self._set_up()
-        r = self.app.put(self.task_path_id_1,
-                         json=self.missing_argument_content)
+        r = self.app.put("/api/task/1/",
+                         json={"title": "some title"})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
     def test_put_missing_arguments_all(self):
-        self._set_up()
-        r = self.app.put(self.task_path_id_1,
-                         json=self.missing_arguments_all)
+        r = self.app.put("/api/task/1/",
+                         json={})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
     def test_put_title_less_chars(self):
-        self._set_up()
-        r = self.app.put(self.task_path_id_1,
-                         json=self.title_less_chars)
+        r = self.app.put("/api/task/1/",
+                         json={"title": "", "content": "some content"})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
     def test_put_content_less_chars(self):
-        self._set_up()
-        r = self.app.put(self.task_path_id_1,
-                         json=self.content_less_chars)
+        r = self.app.put("/api/task/1/",
+                         json={"title": "some title", "content": ""})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
     def test_put_title_more_chars(self):
-        self._set_up()
-        r = self.app.put(self.task_path_id_1,
-                         json=self.title_more_chars)
+        r = self.app.put("/api/task/1/",
+                         json={"title": "s" * 151, "content": "some content"})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
     def test_put_typo_title(self):
-        self._set_up()
-        r = self.app.put(self.task_path_id_1,
-                         json=self.typo_title)
+        r = self.app.put("/api/task/1/",
+                         json={"tutle": "some title", "content": "some content"})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
     def test_put_typo_content(self):
-        self._set_up()
-        r = self.app.put(self.task_path_id_1,
-                         json=self.typo_content)
+        r = self.app.put("/api/task/1/",
+                         json={"title": "some title", "cuntent": "some content"})
         self.assertEqual(r.status_code, 400)
-        self._tear_down()
 
 
 if __name__ == '__main__':
